d9731366245f7b5165aa7765796e862e21423656
Coffeelot <slutastalkamig@gmail.com>
2025-12-06 18:34:49 +0100

    fix: sidebar tooltip

--- web/src/components/app/SidebarUser.vue
+++ web/src/components/app/SidebarUser.vue
@@ -8,25 +8,27 @@
       >
         <SidebarTrigger />
         <div class="grid flex-1 text-left text-sm leading-tight gap-1">
-          <span class="truncate font-semibold">
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger class="flex items-center gap-2">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger as-child>
+                <div class="flex items-center gap-2 truncate font-semibold">
                   {{ globalStore.baseData?.data?.currentRacerName }}
                   <span v-if="globalStore.baseData?.data?.currentCrewName">
                     [{{ globalStore.baseData.data.currentCrewName }}]
                   </span>
-                </TooltipTrigger>
-                <TooltipContent side="bottom">
+                </div>
+              </TooltipTrigger>
+              <TooltipContent class="dark" side="bottom">
+                <div class="flex items-center gap-2">
                   {{ globalStore.baseData?.data?.currentRacerName }}
                   <span v-if="globalStore.baseData?.data?.currentCrewName">
                     [{{ globalStore.baseData.data.currentCrewName }}]
                   </span>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          </span>
-          <span class="flex items-center gap-1">
+                </div>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <div class="flex items-center gap-1">
             <Badge v-if="globalStore.baseData?.data?.currentRacerAuth" variant="outline">
               <UserSpecificIcon />
               {{ translate("auth_type_" + globalStore.baseData?.data?.currentRacerAuth) }}
@@ -39,7 +41,7 @@
               {{ globalStore.baseData?.data?.currentVehicle?.model }}
               [{{ globalStore.baseData?.data?.currentVehicle?.class }}]
             </Badge>
-          </span>
+          </div>
         </div>
       </SidebarMenuButton>
     </SidebarMenuItem>
@@ -54,7 +56,6 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
 import {
   SidebarMenu,
   SidebarMenuButton,
@@ -68,8 +69,7 @@ import CryptoDialog from "./components/dialogs/CryptoDialog.vue";
 import { Badge } from "../ui/badge";
 import { CarIcon } from "lucide-vue-next";
 import UserSpecificIcon from "./components/UserSpecificIcon.vue";
-import { TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
-import Tooltip from "../ui/tooltip/Tooltip.vue";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
 
 const sidebarContext = useSidebar();
 
